01d52534bcec9824860f02b2730a1e17dfa7cd47
migmol04 <147338662+migmol04@users.noreply.github.com>
2023-10-10 00:22:34 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 package tp1;
+import tp1.view.GamePrinter;
+import tp1.logic.gameobjects.*;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -27,43 +29,55 @@ public class Main {
 	 * @param args Arguments for the game.
 	 */
 	public static void main(String[] args) {
-		// Required to avoid issues with tests
-		Locale.setDefault(new Locale("es", "ES"));
+	    Locale.setDefault(new Locale("es", "ES"));
 
-		if (args.length < 1 || args.length > 2) {
-			usage();
-		} else {
-			Level level = Level.valueOfIgnoreCase(args[0]);
-			if (level == null) {
-				System.out.println(Messages.ALLOWED_LEVELS);
-				usage();
-			} else {
-				long seed = System.currentTimeMillis() % 1000;
-				String seedParam = "";
-				try {
-					if (args.length == 2) {
-						seedParam = args[1];
-						seed = Long.parseLong(seedParam);
-					}
-		
-					System.out.println(Messages.WELCOME);
-		
-					System.out.println(String.format(Messages.CONFIGURED_LEVEL, level.name()));
-					System.out.println(String.format(Messages.CONFIGURED_SEED, seed));
-		
-					Game game = new Game(level, seed);
-					Scanner scanner = new Scanner(System.in);
-					Controller controller = new Controller(game, scanner);
-					controller.run();
-		
-				} catch (NumberFormatException nfe) {
-					System.out.println(String.format(Messages.SEED_NOT_A_NUMBER_ERROR, seedParam));
-					usage();
-				} catch (Exception e) {
-					System.out.println(error(e.getMessage()));
-				}
-			}
-		}
+	    if (args.length < 1 || args.length > 2) {
+	        usage();
+	    } else {
+	        Level level = Level.valueOfIgnoreCase(args[0]);
+	        if (level == null) {
+	            System.out.println(Messages.ALLOWED_LEVELS);
+	            usage();
+	        } else {
+	            long seed = System.currentTimeMillis() % 1000;
+	            String seedParam = "";
+	            try {
+	                if (args.length == 2) {
+	                    seedParam = args[1];
+	                    seed = Long.parseLong(seedParam);
+	                }
+
+	                System.out.println(Messages.WELCOME);
+
+	                System.out.println(String.format(Messages.CONFIGURED_LEVEL, level.name()));
+	                System.out.println(String.format(Messages.CONFIGURED_SEED, seed));
+
+	                Game game = new Game(level, seed);
+	                Scanner scanner = new Scanner(System.in);
+	                Controller controller = new Controller(game, scanner);
+
+	                // Crear una instancia de UCMShip y agregarla al juego
+	                UCMShip ucmShip = game.getUCMShip();
+	                // Configura la instancia de UCMShip como sea necesario, por ejemplo, estableciendo su posición
+
+	                // Crear una instancia de GamePrinter
+	                GamePrinter gamePrinter = new GamePrinter(game);
+
+	                // Inicia el juego y el bucle principal
+	                controller.run(); // Este es el bucle principal
+
+	                // Después del bucle principal, imprime el tablero final
+	                String finalGameBoard = gamePrinter.toString();
+	                System.out.println(finalGameBoard);
+
+	            } catch (NumberFormatException nfe) {
+	                System.out.println(String.format(Messages.SEED_NOT_A_NUMBER_ERROR, seedParam));
+	                usage();
+	            } catch (Exception e) {
+	                System.out.println(error(e.getMessage()));
+	            }
+	        }
+	    }
 	}
 
 }
